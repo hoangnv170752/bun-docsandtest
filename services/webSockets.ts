47441f28c0997ecd78f11dbbc71f5b5114fe4c1f
hoangnv170752 <hoang.nv.ral@gmail.com>
2024-08-31 15:54:33 +0700

    bun routing

--- services/webSockets.ts
+++ services/webSockets.ts
@@ -1,11 +1,29 @@
+import {watch} from 'fs';
+
 export const serverSocket = Bun.serve<{ authToken: string }>({
-    fetch(req, server) {
+    async fetch(req, server) {
       const success = server.upgrade(req);
+      watch(import.meta.dir, (event, fileName)=>{
+        console.log('Detected upgrade ' + fileName + ' from ' + event);
+      })
       if (success) {
         return undefined;
       }
+      const path = new URL(req.url).pathname;
+
+      if (path === "/") {
+        return new Response("Welcome to the Bun environment!");
+      }
+
+      if (req.method === "POST" && path === "/api/post") {
+        const data = await req.json();
+        console.log("Received JSON:", data);
+        return Response.json({ success: true, data });
+      }
   
-      return new Response("Hello world!");
+      else {
+        return new Response("Hello world!");
+      }
     },
     websocket: {
       async message(ws, message) {
